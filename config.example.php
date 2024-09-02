9e91546e8454c0c36f448ee31e940b7cfbc41ddd
TomTeisman <tjg.teisman@gmail.com>
2024-09-02 21:56:58 +0200

    updated comments within config

--- config.example.php
+++ config.example.php
@@ -1,9 +1,28 @@
 <?php
-define("DB_SERVER", "");    // Server address
-define("DB_USERNAME", "");  // Username for the database
-define("DB_PASSWORD", "");  // Password for the database
-define("DB_NAME", "");      // Name of the database
-define("ROOT_PATH", "");    // Path to the project's root
+/**
+ * The server ip where the database lives
+ */
+define("DB_SERVER", "");
+
+/**
+ * The username for the database
+ */
+define("DB_USERNAME", "");
+
+/**
+ * The password for the database
+ */
+define("DB_PASSWORD", "");
+
+/**
+ * The name of the database
+ */
+define("DB_NAME", "");
+
+/**
+ * The path to the root of this project
+ */
+define("ROOT_PATH", "");
 
 try {
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
